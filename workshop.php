f5c62efe7dcad5aaef1f6f8756db4f983845bdab
Muhammad Alfian <muhammadalfian500@gmail.com>
2017-03-19 14:16:14 +0700

    add php and result page

--- workshop.php
+++ workshop.php
@@ -5,9 +5,18 @@
 	 * that's why using template engine is considered best practice
 	 * */
 	
-	$now = date('Y-m-d'); // get today's date
-	$html = file_get_contents('workshop.html'); // get html content from other file
-	$html = str_replace('{{date}}', $now, $html); // replace {{date}} with our $now variable
+	
+	
+	if(isset($_GET['nama'])){
+		$nama = $_GET['nama'];
+		$date = date('d-m-Y'); // get today's date
+		$time = time('h:m:s'); // get today's date
+		$html = file_get_contents('workshop-result.html'); // get html content from other file
+		$html = str_replace('{{date}}', $date, $html); // replace {{date}} with our $now variable
+		$html = str_replace('{{time}}', $time, $html); // replace {{date}} with our $now variable
+		$html = str_replace('{{nama}}', $nama, $html); // replace {{date}} with our $now variable
+	
+	}
 	
 	echo $html;
 ?>
