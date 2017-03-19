685d7f4fea1cdc3e0d4678166cd75c7e79ad5f37
Gema Ulama Putra <cybermujahidz@gmail.com>
2017-03-19 14:17:06 +0700

    edit komentar

--- workshop.php
+++ workshop.php
@@ -8,8 +8,8 @@
 	$now = date('Y-m-d'); // get today's date
 	$html = file_get_contents('workshop.html'); // get html content from other file
 	$html = str_replace('{{date}}', $now, $html); // replace {{date}} with our $now variable
-	$html = str_replace('{{member1}}', "Gema", $html); // replace {{date}} with our $now variable
-	$html = str_replace('{{member2}}', "Yaqien", $html); // replace {{date}} with our $now variable
+	$html = str_replace('{{member1}}', "Yaqien", $html); // replace {{member1}} with Yaqien's name
+	$html = str_replace('{{member2}}', "Gema", $html); // replace {{member2}} with Gema's name
 	
 	echo $html;
 ?>
